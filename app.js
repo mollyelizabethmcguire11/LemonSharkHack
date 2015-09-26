10b86cf994ce9d037e2737168683114a2c07dc93
jdomek <jenna_domek@student.uml.edu>
2015-09-26 11:47:57 -0400

    fixed johny five

--- app.js
+++ app.js
@@ -32,14 +32,16 @@ var five = require("johnny-five"),
     onButton, offButton, led;
 	five.Board().on("ready", function() {
 	  onButton = new five.Button(2);
+	  offButton = new five.Button(3);
 	  led = new five.Led(13);
 
 	  onButton.on("down", function(value){
+		global.name = Math.random()
 		led.on();
 	  });
 
-	  offButton = new five.Button(3);
-		offButton.on("down", function(){
+	  offButton.on("down", function(){
 	  led.off();
+	  
 	});
 });
